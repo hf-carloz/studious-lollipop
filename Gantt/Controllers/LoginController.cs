cd43d8eb359fb142de27f3974ec90dc3f52f86a6
dale <dale.nicodemus@fetp.ph>
2017-07-28 10:07:54 +0800

    Login, Register, Database

---
+++ Gantt/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gantt.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            Session["LoggedIn"] = null;
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            Session["LoggedIn"] = 2;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Validate()
+        {
+            string user = Request.Form["username"];
+            string pass = Request.Form["password"];
+
+            if ((user == "admin") && (pass == "admin"))
+            {
+                Session["LoggedIn"] = 1;
+                Session["Username"] = user;
+
+                return Redirect("/Dashboard/Index");
+            }
+            else
+            {
+                return Redirect("/Login/Index");
+            }
+
+        }
+
+        public ActionResult Logout()
+        {
+            Session["LoggedIn"] = null;
+            Session["Username"] = "User";
+            return View("Index");
+        }
+    }
+}
